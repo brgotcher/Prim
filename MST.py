6a4744eb55476eb9a92c32a06358522e85a8f1f3
brgotcher <brgotcher@gmail.com>
2021-05-21 07:01:17 -0500

    changed tuple format

--- MST.py
+++ MST.py
@@ -10,7 +10,7 @@ class DisJSet:
             self.parent[x] = self.find(self.parent[x])
         return self.parent[x]
 
-    def union(self,x,y):
+    def union(self, x, y):
         xset = self.find(x)
         yset = self.find(y)
 
@@ -32,28 +32,26 @@ def kruskal(G):
     num = len(G)
     edges = []
     for i in range(num):
-        for j in range(i+1, num):
+        for j in range(i + 1, num):
             if G[i][j]:
-                edges.append(((i,j), G[i][j]))
+                edges.append(((i, j), G[i][j]))
 
     sorted_edges = []
     for edge in sorted(edges, key=lambda edge: edge[1]):
         sorted_edges.append(edge)
-    return sorted_edges
-
-    # MST = {}
-    # index = 0
-    # while len(MST) < num-1:
-    #     (v1, v2) = sorted_edges[index]
-    #
-    # ds = DisJSet(num)
+    # return sorted_edges
 
 
+    MST = {}
+    index = 0
+    while len(MST) < num - 1:
+        (v1, v2) = sorted_edges[index][0]
+        w = sorted_edges[index][1]
 
 
-g1 =  [[0, 9, 75, 0, 0],
- [9, 0, 95, 19, 42],
- [75, 95, 0, 51, 66],
- [0, 19, 51, 0, 31],
- [0, 42, 66, 31, 0]]
+g1 = [[0, 9, 75, 0, 0],
+      [9, 0, 95, 19, 42],
+      [75, 95, 0, 51, 66],
+      [0, 19, 51, 0, 31],
+      [0, 42, 66, 31, 0]]
 print(kruskal(g1))
